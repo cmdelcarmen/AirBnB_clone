fa9fff752f1c3bbbba3219c29aa95a5b0617aad4
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-10 20:15:57 +0000

    update

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,6 +3,7 @@
 '''
 import os
 import models
+import json
 import unittest
 from time import sleep
 from datetime import datetime
@@ -15,6 +16,14 @@ class TestFileStorage(unittest.TestCase):
     '''unittests for our FileStorage class, testing: __init__(), save(),
     all(), reload(), and attributes from the class.
     '''
+
+    def tearDown(self):
+        '''deletes file used to reload objs'''
+        try:
+            os.remove("file.json")
+        except:
+            pass
+
     def test_file_path(self):
         '''Testing the __file_path attribute from FileStorage by attempting
         to delete the file it is supposed to create, file.json.
@@ -49,6 +58,28 @@ class TestFileStorage(unittest.TestCase):
         dict_ = FileStorage._FileStorage__objects
         self.assertIn("BaseModel." + test_obj.id, dict_)
 
+    def test_all2(self):
+        '''Testing all() method'''
+        test_obj = FileStorage()
+        dict_ = test_obj.all()
+        self.assertTrue(isinstance(dict_, dict))
+
+    def test_new(self):
+        ''' '''
+        test_obj = BaseModel()
+        test_obj2 = FileStorage()
+        test_obj2.new(test_obj)
+        self.assertNotEqual(test_obj2.all(), 0)
+
+    def test_reload2(self):
+        ''' '''
+        obj = BaseModel()
+        storage = FileStorage()
+        obj.name = "Holberton"
+        obj.save()
+        storage._FIleStorage__objects = {}
+        storage.reload()
+        self.assertNotEqual(storage.all(), {})
 
 if __name__ == '__main__':
     unittest.main()
