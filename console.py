da020a2be57f563e6ed796b2688d16c2cb2c63cc
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-11 07:05:22 +0000

    updated if statements show/create

--- console.py
+++ console.py
@@ -48,10 +48,10 @@ class HBNBCommand(cmd.Cmd):
         Example: (hbnb) show BaseModel 1234-1234-1234'''
         cmd_args = command.split(' ')
 
-        if len(cmd_args) == 0:
+        if cmd_args == ['']:
             print('** class name missing **')
 
-        if len(cmd_args) < 2:
+        elif len(cmd_args) < 2:
             print('** instance id missing **')
 
         else:
@@ -72,10 +72,10 @@ class HBNBCommand(cmd.Cmd):
         '''
         cmd_args = command.split(' ')
 
-        if len(cmd_args) == 0:
+        if cmd_args == ['']:
             print('** class name missing **')
 
-        if len(cmd_args) < 2:
+        elif len(cmd_args) < 2:
             print('** instance id missing **')
 
         else:
